1b68b2fdeb167efe4067c123c3fbb3d766228165
pduongpdu99 <pduongpdu99@gmail.com>
2026-08-29 15:46:36 +0700

    update get vietnamese stopword method

--- engine/intent/Util.py
+++ engine/intent/Util.py
@@ -11,3 +11,10 @@ def remove_accent(vietnamese_text: str):
     stripped = ''.join(c for c in nfd if unicodedata.category(c) != 'Mn')
     stripped = stripped.replace('Đ', 'D').replace('đ', 'd')
     return unicodedata.normalize('NFC', stripped)
+
+def get_vietnamese_stopwords():
+    stop_words = []
+    with open(os.path.join(root_directory_path, "data", "stop_words.txt"), "rb") as f:
+        stop_words = [i.strip() for i in f.readlines() if i.strip() != ""]
+
+    return stop_words
