3243d4e2763ed7abc2174b76481d970cafce15f0
pduongpdu99 <pduongpdu99@gmail.com>
2026-08-31 00:38:12 +0700

    rename and update main.py

--- stress.py
+++
@@ -1,8 +0,0 @@
-from engine.intent import Brain, join
-
-brain = Brain()
-brain.learn(join("engine", "intent", "data", "tu_van_phan_mem.aiml"))
-
-while True:
-    ask = input("You: ")
-    print("Bot:",brain.respond(ask))
