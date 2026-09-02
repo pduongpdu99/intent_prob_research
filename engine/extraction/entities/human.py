e80810eb5bfcf9c0914337c2d7b5008447108d28
pduongpdu99 <pduongpdu99@gmail.com>
2026-09-02 12:05:13 +0700

    remove human entities

--- engine/extraction/entities/human.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-class HumanEntity(BaseModel):
-    name: Optional[str]
-    age: Optional[str]
-    address: Optional[str]
-    occupation: Optional[str]
