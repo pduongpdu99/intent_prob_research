83db24c48979f0d13447f5662176dc31e5c31a36
pduongpdu99 <pduongpdu99@gmail.com>
2026-09-16 17:35:04 +0700

    update util

--- engine/Util.py
+++ engine/Util.py
@@ -114,7 +114,6 @@ def update_clustering_model(model, embeddings):
     model.predict(embeddings)
     return model
 
-
 # PATH
 DEFAULT_TEMPLATE_NAME = "required_template"
 
@@ -135,6 +134,7 @@ CACHED_DOMAIN_K_CLUSTER_PATH = join(KNOWLEDGE_DIRECTORY, ".cached", "knowledge_b
 CACHED_KNOWLEDGE_BASE_WITH_EMPTY_PATH = join(KNOWLEDGE_DIRECTORY, ".cached", "knowledge_base","knowledge_base_with_empty.json")
 CACHED_KNOWLEDGE_BASE_ELBOW_PATH = join(KNOWLEDGE_DIRECTORY, ".cached", "knowledge_base","knowledge_base_elbow_cache.json")
 CACHED_KLB_EMBEDDING_JSON_PATH = join(KNOWLEDGE_DIRECTORY, ".cached", "knowledge_base","klb_embedding_average.json")
+CACHED_KLB_CLUSTERED_EMBEDDING_JSON_PATH = join(KNOWLEDGE_DIRECTORY, ".cached", "knowledge_base","klb_clustered_embedding.json")
 
 SOFTWARE_ROLE_PATH = join(KNOWLEDGE_DIRECTORY, "roles", "software-data.json")
 SOFTWARE_DOMAIN_PATH = join(KNOWLEDGE_DIRECTORY, "domain", "software-domain.json")
